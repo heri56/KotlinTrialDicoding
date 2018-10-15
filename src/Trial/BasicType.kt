6eee200dd97633ecf7ea741990b4264b63c5fd28
Heri Prastio <heri.prastio56@gmail.com>
2018-10-15 18:09:55 +0700

    Boolean true false

--- src/Trial/BasicType.kt
+++ src/Trial/BasicType.kt
@@ -3,7 +3,21 @@ package Trial
 val intNumbers = 3
 val toconvert = intNumbers.toDouble()
 val doubleNumbers = 27.5
+val stringNumber = "333"
+val convertStringNumber = stringNumber.toInt()
+
+val booleanTrue = true
+val booleanFalse = false
+
+val a = 3
+val b = 7
+val c = 5
+val d = "1"
+val converStringAngka = d.toInt()
+val x = a < b && b == c
+val y = converStringAngka == 1
 
 fun main(args: Array<String>){
-    println(toconvert)
+    println(x)
+    println(y)
 }
